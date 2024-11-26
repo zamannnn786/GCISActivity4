b1d151339ad09c6c72c737871025988c7deb06cc
nada944 <81313715+nada944@users.noreply.github.com>
2024-11-26 14:42:11 -0800

    Update test_polygon.py

--- test_polygon.py
+++ test_polygon.py
@@ -33,3 +33,19 @@ def test_set_sides():
     testpoly = polygon()
     testpoly.set_sides([5, 5, 5])
     assert testpoly.Sides == [5, 5, 5]
+
+"""this function tests for equality"""
+poly1 = Polygon("Triangle", [3, 3, 3])
+poly2 = Polygon("Triangle", [3, 3, 3])
+print(poly1 == poly2)  
+
+"""this function tests for inequality"""
+poly3 = Polygon("Square", [4, 4, 4, 4])
+print(poly1 != poly3) 
+
+#optional
+"""this function tests with a non-polygon"""
+non_polygon = "Not a polygon"
+print(poly1 != non_polygon)
+
+
